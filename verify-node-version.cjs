513f2acda5fda8a4cfc650d167d37c2935ab8745
prisis <d.bannert@anolilab.de>
2021-06-09 17:22:39 +0200

    fix: removed postinstall feat: added new check to preinstall

--- verify-node-version.cjs
+++ verify-node-version.cjs
@@ -1,13 +1,19 @@
 /* eslint-disable no-console */
 /* eslint-disable unicorn/no-process-exit */
+if (process.env.SKIP_CHECK !== undefined) {
+    process.exit(0);
+}
+
 let requiredVersion = require("fs").readFileSync(".nvmrc", { encoding: "utf8" }).trim();
+
+if (requiredVersion === "") {
+    console.info("No .nvmrc found, skipped check.");
+
+    process.exit(1);
+}
 
 if (!requiredVersion.includes("v")) {
     requiredVersion = `v${requiredVersion}`;
-}
-
-if (process.env.SKIP_CHECK !== undefined) {
-    process.exit(0);
 }
 
 if (process.version.split(".")[0] !== requiredVersion.split(".")[0]) {
