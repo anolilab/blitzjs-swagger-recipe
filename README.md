258fd98cfd2a0a6843c60e248c42e20afca1e4b4
prisis <d.bannert@anolilab.de>
2021-06-09 17:51:31 +0200

    feat: added readme

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # BlitzJS Swagger Recipe
 
-Todo
+A BlitzJs swagger page based on [swagger jsdoc](https://github.com/Surnet/swagger-jsdoc).
+
+## Getting Started
+
+Using the recipe is the easiest way. It will add the packages and create the base files.
+
+**Run in your terminal**
+
+`blitz install anolilab/blitzjs-swagger-recipe`
+
+After the installation is done, you can visit the swagger page on `localhost:3000/swagger`.
+
+## Learn More
+
+To learn more about swagger and swagger jsdoc, take a look at the following resources:
+
+- [Swagger Documentation](https://swagger.io/) - learn about swagger features and API.
+- [Swagger jsdoc](https://github.com/Surnet/swagger-jsdoc) - learn about swagger jsdoc
---
+++ release.config.js
@@ -0,0 +1,61 @@
+module.exports = {
+    "branches": [
+        "+([0-9])?(.{+([0-9]),x}).x",
+        "main",
+        "next",
+        "next-major",
+        {
+            "name": "beta",
+            "prerelease": true
+        },
+        {
+            "name": "alpha",
+            "prerelease": true
+        }
+    ],
+    "dryRun": false,
+    "plugins": [
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                "preset": "conventionalcommits"
+            }
+        ],
+        [
+            "@google/semantic-release-replace-plugin",
+            {
+                "replacements": [
+                    {
+                        "files": [
+                            "composer.json"
+                        ],
+                        "from": "\"dev-main\": \".*\"",
+                        "to": "\"dev-main\": \"${nextRelease.version.replace(/\\.\\w+$/, '-dev')}\""
+                    }
+                ]
+            }
+        ],
+        [
+            "@semantic-release/release-notes-generator",
+            {
+                "preset": "conventionalcommits"
+            }
+        ],
+        "@semantic-release/changelog",
+        [
+            "@semantic-release/github",
+            {
+                "assets": [
+                    "templates/**",
+                    "index.ts",
+                    "package.json",
+                    "CHANGELOG.md",
+                    "LICENSE.md",
+                    "README.md",
+                    "UPGRADE.md"
+                ],
+                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+            }
+        ]
+    ]
+}
